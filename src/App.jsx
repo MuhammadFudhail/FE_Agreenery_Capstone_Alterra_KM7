3f4892f000468cdb10fed0e932ad4ff3f56f3f52
haikrisna <krisnakukuh54@gmail.com>
2024-11-27 13:49:43 +0700

    buat branch feature/chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
     <h1>Ini dari branch feature login register</h1>
     <h1>Ini dari branch feature/landing-page</h1>
+    <h1>Ini dari branch feature/chatbot</h1>
     <Router>
       {/* Navbar Taruh di Apps <Navbar/> */}
       <Routes>
